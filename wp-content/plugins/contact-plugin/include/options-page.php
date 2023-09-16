4a88560591faf19cf6a4424cd3e2c202b9406971
Anmol Verma <anmolverma102002@gmail.com>
2023-09-16 16:49:45 +0530

    feat: :sparkles: updated docs and plugin->Field/Container

--- wp-content/plugins/contact-plugin/include/options-page.php
+++ wp-content/plugins/contact-plugin/include/options-page.php
@@ -22,12 +22,21 @@ function create_options_page()
     /**
      * @package Container create a new field in dashboard of wordpress
      * @package Field takes @param like text/textarea to create input and many more
+     ** @package Field takes @param type, id, and disply text as argument
+     * Function __() is commonly used as a function name for translating text or strings, if user choose different language the text inside __() will be only translated into that language
+     * The order matters
      */
-    Container::make('theme_options', __('Theme Options'))
+    Container::make('theme_options', __('Contact Form'))
+        ->set_icon('dashicons-media-text')
         ->add_fields(
             array(
-                Field::make('text', 'crb_facebook_url', __('Facebook URL')),
-                Field::make('textarea', 'crb_footer_text', __('Footer Text'))
+                Field::make('checkbox', 'contact_plugin_active', __('Active')),
+                Field::make('text', 'contact_plugin_recipients', __('Recipients Email'))
+                    ->set_attribute('placeholder', 'eg. your@email.com')
+                    ->set_help_text('The email that the form is submitted to'),
+                Field::make('textarea', 'contact_plugin_message', __('Conformation Message'))
+                    ->set_attribute('placeholder', 'Enter conformation message')
+                    ->set_help_text('Type the message you want the submitter to recieve')
             )
         );
 }
