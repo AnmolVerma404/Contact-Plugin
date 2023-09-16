02b8191d45d2c6f22f287f4c596da5bb6a499293
Anmol Verma <anmolverma102002@gmail.com>
2023-09-16 15:46:59 +0530

    feat: :sparkles: added options-page on dashboard

--- wp-content/plugins/contact-plugin/include/options-page.php
+++ wp-content/plugins/contact-plugin/include/options-page.php
@@ -1,15 +1,34 @@
 <?php
-
 use Carbon_Fields\Container;
-use Carbon_fields\Field;
+use Carbon_Fields\Field;
 
-/*
+/**
  * This hook allow us to call function load_carbon_field after theme is loded
  */
 add_action('after_setup_theme', 'load_carbon_field');
+add_action('carbon_fields_register_fields', 'create_options_page');
 
 function load_carbon_field()
 {
     \Carbon_Fields\Carbon_Fields::boot();
+}
+
+/**
+ * Create options page in contact plugin.
+ * This page will show in left dashboard of wordpress
+ */
+function create_options_page()
+{
+    /**
+     * @package Container create a new field in dashboard of wordpress
+     * @package Field takes @param like text/textarea to create input and many more
+     */
+    Container::make('theme_options', __('Theme Options'))
+        ->add_fields(
+            array(
+                Field::make('text', 'crb_facebook_url', __('Facebook URL')),
+                Field::make('textarea', 'crb_footer_text', __('Footer Text'))
+            )
+        );
 }
 ?>
