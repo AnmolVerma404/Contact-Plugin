4ae992538faf318cc46a1b9f98457e1889858af5
Anmol Verma <anmolverma102002@gmail.com>
2023-09-16 13:57:59 +0530

    feat: :sparkles: added include utils

--- wp-content/plugins/contact-plugin/contact-plugin.php
+++ wp-content/plugins/contact-plugin/contact-plugin.php
@@ -14,12 +14,33 @@ if (!defined('ABSPATH')) {
     die('You can\'t be here');
 }
 
+/*
+ * class_exists checks if the class is defined
+ */
 if (!class_exists('ContactPlugin')) {
     class ContactPlugin
     {
+        /*
+         * Create a constructer for @class ContactPlugin
+         * Require once will check if the file has already been included and if not it will get the file given the file path
+         * plugin_dir_path is a wordpress function which find's plugin folder form the current dir then in the end we can mention the plugin autoload file
+         */
+        public function __construct()
+        {
+            define('MY_PLUGIN_PATH', plugin_dir_path(__FILE__));
+            require_once(MY_PLUGIN_PATH . '/vendor/autoload.php');
+        }
 
+        /*
+         * To simplify our code and break down it into different files, we have created a utilities class, and we will include it once in the initialize function
+         */
+        public function initialize()
+        {
+            include_once MY_PLUGIN_PATH . 'include/utilities.php';
+        }
     }
 
-    new ContactPlugin;
+    $contactPlugin = new ContactPlugin;
+    $contactPlugin->initialize();
 }
 ?>
