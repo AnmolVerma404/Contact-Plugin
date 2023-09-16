ecf00907408bb5dea85689fb0f7b9665d24118b2
Anmol Verma <anmolverma102002@gmail.com>
2023-09-16 23:23:56 +0530

    feat: :sparkles: added jquery AJAX code, but no resp

--- wp-content/plugins/contact-plugin/include/templates/contact-form.php
+++ wp-content/plugins/contact-plugin/include/templates/contact-form.php
@@ -1,4 +1,4 @@
-<form>
+<form id="enquiry_form">
     <label for>Name</label><br/>
     <input type="text" name="name"><br/><br/>
     <label>Email</label><br/>
@@ -7,6 +7,22 @@
     <input type="text" name="phone"><br/><br/>
     <label>Your Message</label><br/>
     <textarea name="message"></textarea><br/><br/>
+    
+    <button type="submit">Subbmit Form</button>
 </form>
 
-<button type="submit">Subbmit Form</button>
+<script>
+
+    jQuery(document).ready(function($){
+        $("#enquiry_form").submit(function(event){
+            event.preventDefault();
+            var form = $(this);
+            $.ajax({
+                type:'POST',
+                url: "<?php echo get_rest_url(null, "v1/contact-form/subbmit"); ?>",
+                data: form.serialize()
+            })
+        })
+    })
+
+</script>
