b9d1620e9cf402d70aa6fdea74d210da9f2feb42
Anmol Verma <anmolverma102002@gmail.com>
2023-09-21 13:53:03 +0530

    feat: :sparkles: connected contact-form with backend

--- wp-content/plugins/contact-plugin/include/templates/contact-form.php
+++ wp-content/plugins/contact-plugin/include/templates/contact-form.php
@@ -1,43 +1,47 @@
-<div id="form_success" style="background:green; color: white;"></div>
-<div id="form_error" style="background:red; color: white;"></div>
+<?php if (get_plugin_options('contact_plugin_active')): ?>
+    <div id="form_success" style="background:green; color: white;"></div>
+    <div id="form_error" style="background:red; color: white;"></div>
 
-<form id="enquiry_form">
+    <form id="enquiry_form">
 
 
-    <?php wp_nonce_field('wp_rest') ?>
+        <?php wp_nonce_field('wp_rest') ?>
 
-    <label for>Name</label><br/>
-    <input type="text" name="name"><br/><br/>
-    <label>Email</label><br/>
-    <input type="text" name="email"><br/><br/>
-    <label>Phone</label><br/>
-    <input type="text" name="phone"><br/><br/>
-    <label>Your Message</label><br/>
-    <textarea name="message"></textarea><br/><br/>
+        <label for>Name</label><br/>
+        <input type="text" name="name"><br/><br/>
+        <label>Email</label><br/>
+        <input type="text" name="email"><br/><br/>
+        <label>Phone</label><br/>
+        <input type="text" name="phone"><br/><br/>
+        <label>Your Message</label><br/>
+        <textarea name="message"></textarea><br/><br/>
     
-    <button type="submit">Submit Form</button>
-</form>
+        <button type="submit">Submit Form</button>
+    </form>
 
-<script>
+    <script>
 
-    jQuery(document).ready(function($){
-        $("#enquiry_form").submit(function(event){
-            event.preventDefault();
-            var form = $(this);
-            console.log(form.serialize());
-            $.ajax({
-                type:'POST',
-                url: "<?php echo get_rest_url(null, "v1/contact-form/submit"); ?>",
-                data: form.serialize(),
-                success: function(res){
-                    form.hide();
-                    $("#form_success").html(res).fadeIn();
-                },
-                error: function(){
-                    $("#form_error").html("There was an error submitting your form").fadeIn();
-                }
+        jQuery(document).ready(function($){
+            $("#enquiry_form").submit(function(event){
+                event.preventDefault();
+                var form = $(this);
+                console.log(form.serialize());
+                $.ajax({
+                    type:'POST',
+                    url: "<?php echo get_rest_url(null, "v1/contact-form/submit"); ?>",
+                    data: form.serialize(),
+                    success: function(res){
+                        form.hide();
+                        $("#form_success").html(res).fadeIn();
+                    },
+                    error: function(){
+                        $("#form_error").html("There was an error submitting your form").fadeIn();
+                    }
+                })
             })
         })
-    })
 
-</script>
+    </script>
+<?php else: ?>
+        This form is not active
+<?php endif; ?>
